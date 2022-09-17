b3197cb09f31bf6bb8ba3f139cdb58f7da65cf0c
DanielBretherick <68389495+DanielBretherick@users.noreply.github.com>
2022-09-17 10:57:48 -0300

    Versão Final
    Versão final do programa.

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ public class Main {
     public static void main(String[] args) {
         MySqlConnection.List();
         LinkedList<Airport> airports = MySqlConnection.generateAirports();
-//        for(int i =0; i< airports.size(); i++){
-//            System.out.println(airports.get(i).getLatitude());
-//        }
+
         Graph teste = new Graph(airports);
 
         System.out.println("Digite o aeroporto de saída:");
@@ -16,12 +14,10 @@ public class Main {
         String aeroIn = scanner.next();
         System.out.println("Digite o aeroporto de chegada:");
         String aeroOut = scanner.next();
-        System.out.println("Saíndo de  " + aeroIn + " ---->" + "Chegando em  " + aeroOut);
+        System.out.println("Saíndo de  " + aeroIn + " ---->" + " Chegando em  " + aeroOut);
 
         String result = teste.dijkstraAlg(aeroIn, aeroOut);
         MySqlConnection.addRequirement(aeroIn, aeroOut, result);
-
-
 
     }
 }
--- src/MySqlConnection.java
+++ src/MySqlConnection.java
@@ -2,14 +2,20 @@ import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
 
+
+
 public class MySqlConnection {
+    static String password = "Jr3d8492gz";
+    static String user = "root";
     public static void List(){
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcvideo", "root", "Jr3d8492gz");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcvideo", user, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from airport_data");
+            ResultSet resultSet = statement.executeQuery("select * from planilhaAeroportos");
+            System.out.println("Aero" + "    "+ "Estado"+ "     " + "Cidade" );
+            System.out.println("-----------------------------");
             while(resultSet.next()){
-                System.out.println(resultSet.getString("iata") + "    "+ resultSet.getString("state") );
+                System.out.println(resultSet.getString("initials") + "    "+ resultSet.getString("state")+"     " + resultSet.getString("city") );
             }
 
         } catch(Exception e){
@@ -18,14 +24,13 @@ public class MySqlConnection {
     }
     public static LinkedList<Airport> generateAirports(){
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcvideo", "root", "Jr3d8492gz");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcvideo", user, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from airport_data");
+            ResultSet resultSet = statement.executeQuery("select * from planilhaAeroportos");
             LinkedList<Airport> airports = new LinkedList<Airport>();
             while(resultSet.next()) {
-                Airport aeroporto = new Airport(resultSet.getString("iata"), resultSet.getFloat("latitude"), resultSet.getFloat("longitude"));
+                Airport aeroporto = new Airport(resultSet.getString("initials"), resultSet.getFloat("latitude"), resultSet.getFloat("longitude"));
                 airports.add(aeroporto);
-                System.out.println(resultSet.getString("iata") + "    " + resultSet.getString("state"));
             }
             return airports;
         } catch(Exception e){
@@ -36,7 +41,7 @@ public class MySqlConnection {
     }
     public static void addRequirement(String aeroIn, String aeroOut, String path){
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcvideo", "root", "Jr3d8492gz");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcvideo", user, password);
             Statement stmt;
             stmt = connection.createStatement();
             stmt.execute("INSERT INTO search (saida,chegada,caminho) "
